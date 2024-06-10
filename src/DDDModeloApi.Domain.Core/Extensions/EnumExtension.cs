4f206e48f3122eb19a9c9da59555328d0ac7e2e4
Renan de Carvalho Ribeiro <renancr176@gmaail.com>
2024-06-10 18:05:00 -0300

    Continuacao da criacao Domain

---
+++ src/DDDModeloApi.Domain.Core/Extensions/EnumExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDModeloApi.Domain.Core.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static T? GetAttributeOfType<T>(this Enum enumValue) where T : Attribute
+        {
+            var type = enumValue.GetType();
+            var memInfo = type.GetMember(enumValue.ToString()).First();
+            var attributes = memInfo.GetCustomAttributes<T>(false);
+            return attributes.FirstOrDefault();
+        }
+    }
+}
---
+++ src/DDDModeloApi.Domain.Core/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDModeloApi.Domain.Core.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        public static Expression<Func<T, bool>> AndAlso<T>(
+            this Expression<Func<T, bool>> left,
+            Expression<Func<T, bool>> right)
+        {
+            var param = Expression.Parameter(typeof(T), "x");
+            var body = Expression.AndAlso(
+                Expression.Invoke(left, param),
+                Expression.Invoke(right, param)
+            );
+            var lambda = Expression.Lambda<Func<T, bool>>(body, param);
+            return lambda;
+        }
+
+        public static Expression<Func<T, bool>> OrElse<T>(
+            this Expression<Func<T, bool>> left,
+            Expression<Func<T, bool>> right)
+        {
+            var param = Expression.Parameter(typeof(T), "x");
+            var body = Expression.OrElse(
+                Expression.Invoke(left, param),
+                Expression.Invoke(right, param)
+            );
+            var lambda = Expression.Lambda<Func<T, bool>>(body, param);
+            return lambda;
+        }
+    }
+}
